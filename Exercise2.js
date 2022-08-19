aefdde6af25d095b63da5896882ca0d678759651
Karla Sequen <karla.sequen@megapaca.com>
2022-08-18 22:27:57 -0600

    exercise 2

--- Exercise2.js
+++ Exercise2.js
@@ -1,3 +1,20 @@
 /*
 
+Requirement: Given a text, verify if its a palindrome
+
+Steps:
+- create a function that receives a text as a parameter
+- create a new word with reverse the string (as an array) and compare with the original word
+
+Test cases:
+- isPalindrome("otto") // true
+- isPalindrome("victor") // false
+
 */
+
+function isPalindrome(text) {
+    return text === text.split("").reverse().join('')
+}
+
+console.log(isPalindrome("otto"));
+console.log(isPalindrome("victor"));
