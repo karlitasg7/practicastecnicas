a0b57185f67f8667a58391154c6033a4f1a9399e
Karla Sequen <karlasequen@gmail.com>
2022-08-22 21:21:35 -0600

    exercise 13

---
+++ Exercise13.js
@@ -0,0 +1,42 @@
+/*
+
+Requirement: Given an array, separate in X arrays, based in a number that indicate the size
+
+Test Cases:
+    divideArray([7,8,9,10], 2); // return [7,8], [9,10]
+
+Steps:
+    - create a function that receives an array an a number as a parameter
+    - create a variable to save the array of arrays (for response)
+    - make a while the array has elements
+    - inside the while make another while from 0 to the size number (parameter)
+    - get the element and push in new array
+    - add new array to the principal array (array of arrays)
+    - return the array of arrays
+
+*/
+
+function divideArray(arr, size) {
+    let arrayOfArrays = [];
+
+    while (arr.length > 0) {
+        let i = 0;
+        let newArray = [];
+        while (i < size) {
+
+            if (arr.length == 0) break;
+
+            newArray.push(arr[0]);
+            arr.shift();
+
+            i++;
+        }
+        arrayOfArrays.push(newArray);
+    }
+
+    return arrayOfArrays;
+
+}
+
+console.log(divideArray([7, 8, 9, 10], 2)); // return [7,8], [9,10]
+console.log(divideArray([7, 8, 9, 10, 5], 2)); // return [7,8], [9,10], [5]
