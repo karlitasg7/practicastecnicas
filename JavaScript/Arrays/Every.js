a518e51ef49ef1b7adc51da47154e6766d14b436
Karla Sequen <karlasequen@gmail.com>
2023-04-03 15:01:56 -0600

    Test find and every in arrays with JS

---
+++ JavaScript/Arrays/Every.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5];
+const allPositive = numbers.every(element => element > 0);
+console.log(allPositive);
+// Output: true
---
+++ JavaScript/Arrays/Find.js
@@ -0,0 +1,12 @@
+/*
+
+Find return the first element with the condition
+return undefined if not exist any element
+
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const result = numbers.find(number => number > 5);
+
+console.log(result);
+// Output: 6
