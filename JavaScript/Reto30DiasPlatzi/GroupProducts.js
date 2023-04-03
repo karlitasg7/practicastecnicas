6187b8e276d58a77e7273e95775d9021103bdffc
Karla Sequen <karlasequen@gmail.com>
2023-04-03 16:52:10 -0600

    group products using filter, map and reduce

---
+++ JavaScript/Reto30DiasPlatzi/GroupProducts.js
@@ -0,0 +1,48 @@
+/*
+
+En este desafío, tendrás la tarea de agrupar una lista de productos según su categoría.
+
+Para ello, debes implementar la lógica de la función groupProducts que recibirá dos parámetros: products y category.
+
+El primer parámetro products es una lista de objetos que representan cada producto y contienen las propiedades:
+name, category y price. El segundo parámetro category específica a qué categoría se filtrarán los productos.
+
+La función debe retornar un objeto con dos propiedades: products que contiene la cadena de texto con los nombres de los
+productos respetando el orden en el que llegan separados por comas,
+y totalPrice que contiene la suma total de los precios.
+
+ */
+
+function groupProducts(products, category) {
+
+    const filteredProducts = products
+        .filter(product => product.category === category)
+        .map(product => product.name);
+
+    const totalPrice = products
+        .filter(product => product.category === category)
+        .reduce((sum, product) => sum += product.price, 0);
+
+    return {
+        products: filteredProducts.join(", "),
+        totalPrice,
+    };
+}
+
+console.log(
+    groupProducts([
+        {name: "Smartphone", category: "Electronics", price: 800},
+        {name: "Laptop", category: "Electronics", price: 1200},
+        {name: "Shirt", category: "Clothing", price: 50},
+        {name: "Pants", category: "Clothing", price: 100},
+    ], "Electronics")
+);//Output: { products: "Smartphone, Laptop", totalPrice: 2000, }
+
+console.log(
+    groupProducts([
+        {name: "Smartphone", category: "Electronics", price: 800},
+        {name: "Laptop", category: "Electronics", price: 1200},
+        {name: "Shirt", category: "Clothing", price: 50},
+        {name: "Pants", category: "Clothing", price: 100},
+    ], "Clothing")
+); // Output: { products: "Shirt, Pants", totalPrice:150,}
