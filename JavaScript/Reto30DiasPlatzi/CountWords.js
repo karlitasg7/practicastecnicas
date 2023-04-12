f214defa7aa01cf403f57c7a01b5c52e9a56ba7f
Karla Sequen <karlasequen@gmail.com>
2023-04-11 21:26:00 -0600

    js count words

---
+++ JavaScript/Reto30DiasPlatzi/CountWords.js
@@ -0,0 +1,35 @@
+/*
+
+En este desafío tienes array de strings y cada línea es un fragmento, el Zen de Python, tu reto es hacer un método que retorne el número de palabras totales que tiene el array.
+
+Para solucionarlo vas a encontrar una función llamada countWords que tiene un parámetro de entrada:
+
+array: Un array de frases
+Dentro del cuerpo de la función countWords debes escribir tu solución.
+
+Input: [
+  "Beautiful is better than ugly",
+  "Explicit is better than implicit",
+  "Simple is better than complex",
+  "Complex is better than complicated",
+]
+
+Output: 20
+
+ */
+
+function countWords(array) {
+
+    return array.flatMap(x => x.split(" ")).length;
+
+}
+
+console.log(countWords(
+        [
+            "Beautiful is better than ugly",
+            "Explicit is better than implicit",
+            "Simple is better than complex",
+            "Complex is better than complicated",
+        ]
+    )
+);
