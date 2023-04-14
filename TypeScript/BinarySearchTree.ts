5c55e70829e38806ebdd060ffa040add196ac295
Karla Sequen <karlasequen@gmail.com>
2023-04-14 17:51:55 -0600

    binary search tree in typescript

---
+++ TypeScript/BinarySearchTree.ts
@@ -0,0 +1,22 @@
+function bs_list(haystack: number[], needle: number): boolean {
+
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
+
+    return false;
+}
+
+console.log(bs_list([1, 3, 5, 8, 9], 3));
