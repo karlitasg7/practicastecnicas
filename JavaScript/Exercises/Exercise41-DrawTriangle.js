7cddacceaef178b53904a31b5a670c8cc84b99db
Karla Sequen <karlasequen@gmail.com>
2023-06-06 10:25:33 -0600

    exercise 41 js, draw triangle

---
+++ JavaScript/Exercises/Exercise41-DrawTriangle.js
@@ -0,0 +1,40 @@
+/*
+
+Requirement: Given a number, draw a triangle using * and blank spaces with the number of rows based on number
+
+Test Cases:
+    draw(4);
+
+    output
+    *
+   ***
+  *****
+ *******
+
+Steps:
+    - create a function that receives a number
+    - make a for to draw rows
+    - make a for to draw columns with *
+
+*/
+
+function draw(number) {
+
+    let middle = Math.floor(number - 1);
+
+    for (let row = 0; row < number; row++) {
+        let rowContent = "";
+        for (let col = 0; col < (2 * number - 1); col++) {
+
+            if (middle - row <= col && middle + row >= col) {
+                rowContent += "*";
+            } else {
+                rowContent += " ";
+            }
+        }
+        console.log(rowContent);
+    }
+
+}
+
+draw(4);
