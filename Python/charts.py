92980136a3cfa3017296bef21c03dda2e9e24e3b
Karla Sequen <karlasequen@gmail.com>
2023-07-19 19:22:11 -0600

    test charts with bokeh in python

---
+++ Python/charts.py
@@ -0,0 +1,19 @@
+import random
+from bokeh.plotting import figure, output_file, show
+
+if __name__ == '__main__':
+    output_file('graficado_simple.html')
+    fig = figure()
+
+    total_vals = int(input('Cuantos valores quieres graficar?'))
+    x_vals = list(range(total_vals))
+    y_vals = []
+
+    # for x in x_vals:
+    #     val = int(input(f'Valor y para {x}'))
+    #     y_vals.append(val)
+
+    y_vals = [random.randint(0, 1000) for i in range(total_vals)]
+
+    fig.line(x_vals, y_vals, line_width=2)
+    show(fig)
