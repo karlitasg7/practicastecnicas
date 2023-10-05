185a5adf9223a461a4160963b089f1fbfe859a82
Karla Sequen <karlasequen@gmail.com>
2023-10-04 18:21:25 -0600

    leet code exercise 14 longest common prefix

---
+++ JAVA/LeetCode__0014__LongestCommonPrefix/Main.java
@@ -0,0 +1,12 @@
+package LeetCode__0014__LongestCommonPrefix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.longestCommonPrefix(new String[]{"flower", "flow", "flight"})); // "fl"
+        System.out.println(solution.longestCommonPrefix(new String[]{"dog", "racecar", "car"})); //
+    }
+
+}
